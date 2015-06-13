fad42526f7822e9b0cfa168e9f3fabad1489d56a
danielglatstein <danielglatstein@gmail.com>
2015-06-13 01:19:22 -0400

    update format to read easier

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,130 +1,135 @@
 module TicTacToe
 
-  g = Game.new
-  g.play
+      class Game
 
-  class Board
+            def inizialize
+              @symbol1 = "X"
+              @symbol2 = "O"
+            end
 
-    def initialize 
-      @board = Array.new(9) 
-    end
+            def play
+              turn = 0 
+              while winner? && draw? != true
+                @board.show
+                turn
+                symbol_prompt
+                get_symbol
+                cell_prompt
+                get_move
+                update_cell
+                if status.winner? == true
+                  puts "congrats_winner"
+                elsif status.draw? == true
+                  restart?
+                else 
+                  turn += 1
+                end
+              end
+            end
 
-    def show
-      @board.map.with_index do |playermove, index|
-        if index == 0 && playermove.nil?
-          print("#{index} |") 
-        elsif index == 2 && playermove.nil?
-          print("| #{index}")
-        elsif index == 3 && playermove.nil?
-          print("#{index} |") 
-        elsif index == 5 && playermove.nil?
-          print("| #{index}")
-        elsif index == 6 && playermove.nil?
-          print("#{index} |")
-        elsif index == 8 && playermove.nil?
-          print("| #{index}")
-        elsif playermove.nil?
-          print(" #{index} ")
-        else
-          print(" #{playermove} ")
-        end
-        if (index + 1) % 3 == 0 && index < 7
-          print "\n---------\n"
-        end
+
+            def update_cell (symbol, move)
+              @board.map.with_index do |playermove, index|
+                if @move == index
+                  @board[index] = "#{@symbol}" 
+                end
+              end
+            end
+
+            def symbol_prompt
+               puts "#{current_player}, please enter your symbol:"
+            end
+
+            def get_symbol(symbol)
+              @symbol = gets.chomp.to_s
+            end
+
+            def cell_prompt
+              puts "#{current_player}, please enter the fixnum of the cell you would like to occupy:"
+            end
+              
+            end
+
+            def get_move(move)
+              @move = gets.chomp.to_s
+            end
+
+            def turn
+              if turn.even == true
+               @current_player = @player1
+              else
+               @current_player = @player2
+              end
+            end
+
+            def restart?
+              puts @board.new
+            end
+
       end
-    end
 
-    def draw?
-      @board.map do |playermove|
-        if playermove.contain(nil) 
-          false
-        elsif playermove.contain(nil) == false && winner? == false
-          true
+      class Board
+
+        def initialize 
+          @board = Array.new(9) 
         end
-      end
-      draw?
-    end
 
-    def winner?
-      if @board.map 
-    end
-
-  end
-
-  class Player
-    
-    def inizialize
-      @player1 = Player.new(player1)
-      @player2 = Player.new(player2)
-    end
-
-    def symbol
-      @player1.symbol = @symbol1
-      @player2.symbol = @symbol2
-    end
-
-  end
-
-  class Game
-
-    def inizialize
-      @symbol1 = "X"
-      @symbol2 = "O"
-    end
-
-    def play
-      turn = 0 
-      while winner? && draw? != true
-        @board.show
-        turn
-        symbol_prompt
-        get_symbol
-        cell_prompt
-        get_move
-        update_cell
-        if status.winner? == true
-          puts "congrats_winner"
-        elsif status.draw? == true
-          restart?
-        else turn += 1
-    end
-
-    def update_cell (@symbol, @move)
-      @board.map.with_index do |playermove, index|
-        if @move == index
-          @board[index] = "#{@symbol}" 
+        def show
+          @board.map.with_index do |playermove, index|
+            if index == 0 && playermove.nil?
+              print("#{index} |") 
+            elsif index == 2 && playermove.nil?
+              print("| #{index}")
+            elsif index == 3 && playermove.nil?
+              print("#{index} |") 
+            elsif index == 5 && playermove.nil?
+              print("| #{index}")
+            elsif index == 6 && playermove.nil?
+              print("#{index} |")
+            elsif index == 8 && playermove.nil?
+              print("| #{index}")
+            elsif playermove.nil?
+              print(" #{index} ")
+            else
+              print(" #{playermove} ")
+            end
+            if (index + 1) % 3 == 0 && index < 7
+              print "\n---------\n"
+            end
+          end
         end
+
+        def draw?
+          @board.map do |playermove|
+            if playermove.contain(nil) 
+              false
+            elsif playermove.contain(nil) == false && winner? == false
+              true
+            end
+          end
+          draw?
+        end
+
+        def winner?
+          if @board.map 
+        end
+
       end
-    end
 
-    def symbol_prompt
-       puts "#{current_player}, please enter your symbol:"
-    end
+      class Player
+        
+        def inizialize
+          @player1 = Player.new(player1)
+          @player2 = Player.new(player2)
+        end
 
-    def get_symbol(@symbol)
-      @symbol = gets.chomp.to_s
-    end
+        def symbol
+          @player1.symbol = @symbol1
+          @player2.symbol = @symbol2
+        end
 
-    def cell_prompt
-      puts "#{current_player}, please enter the fixnum of the cell you would like to occupy:"
-    end
-      
-    end
-
-    def get_move(@move)
-      @move = gets.chomp.to_s
-    end
-
-    def turn
-      if turn.even == true
-       @current_player = @player1
-      else
-       @current_player = @player2
       end
-    end
 
-    def restart?
-      puts @board.new
-    end
+end
 
-  end
+  
